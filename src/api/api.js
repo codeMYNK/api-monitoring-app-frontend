924183e3e6a03b04ec30db3a0106d43020186051
Mayank Kumar <mynk8076@gmail.com>
2026-09-16 23:43:31 +0530

    Production fixes

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 // const API_BASE_URL = import.meta?.env?.VITE_API_BASE_URL ?? '/api';
+const API_BASE_URL = "https://server-api-app-latest.onrender.com/";
 
-const API_BASE_URL = import.meta?.env?.VITE_API_BASE_URL || 
-    (import.meta?.env?.MODE === 'production' 
-        ? 'https://server-api-app-latest.onrender.com/api' 
-        : '/api');
+// const API_BASE_URL = import.meta?.env?.VITE_API_BASE_URL || 
+//     (import.meta?.env?.MODE === 'production' 
+//         ? 'https://server-api-app-latest.onrender.com/api' 
+//         : '/api');
 
 const api = axios.create({
     baseURL: API_BASE_URL,
